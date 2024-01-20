96d2f7af0c11ef6dda2c1fb89e86ec8aba2adbdb
Zen Dharma <zendharma10@gmail.com>
2024-01-20 10:48:08 +0800

    Change email regex to a better one

--- app/src/main/java/com/zen/nottwitter/domain/StringExtensions.kt
+++ app/src/main/java/com/zen/nottwitter/domain/StringExtensions.kt
@@ -1,6 +1,12 @@
 package com.zen.nottwitter.domain
 
 fun String.isValidEmail(): Boolean {
-    val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$"
+    val emailRegex = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+            "\\@" +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+            "(" +
+            "\\." +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+            ")+"
     return this.matches(emailRegex.toRegex())
 }
