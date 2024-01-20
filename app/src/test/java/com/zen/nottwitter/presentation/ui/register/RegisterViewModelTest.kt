75832aea605a2502666b906bb7571cf2096f3a0c
Zen Dharma <zendharma10@gmail.com>
2024-01-20 10:13:31 +0800

    Add tests for RegisterViewModel

--- app/src/test/java/com/zen/nottwitter/presentation/ui/register/RegisterViewModelTest.kt
+++ app/src/test/java/com/zen/nottwitter/presentation/ui/register/RegisterViewModelTest.kt
@@ -1,4 +1,4 @@
-package com.zen.nottwitter.presentation.ui.login
+package com.zen.nottwitter.presentation.ui.register
 
 import app.cash.turbine.test
 import com.zen.nottwitter.data.model.User
@@ -14,17 +14,17 @@ import io.mockk.spyk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class LoginViewModelTest {
+class RegisterViewModelTest {
 
     private val testDispatchers: TestDispatchers = TestDispatchers()
-    private lateinit var viewModel: LoginViewModel
-    private lateinit var state: StateFlow<LoginUIState>
+    private lateinit var viewModel: RegisterViewModel
+    private lateinit var state: StateFlow<RegisterUIState>
 
     @MockK
     private lateinit var userRepository: UserRepository
@@ -32,6 +32,7 @@ class LoginViewModelTest {
     @get:Rule
     val mainCoroutineRule = MainCoroutineRule(testDispatchers.testDispatcher)
 
+    private val stubTestNickname = "Bob"
     private val stubTestEmail = "test@gmail.com"
     private val stubTestPassword = "test-password"
     private val stubTestUser = User("abc", "Bob", stubTestEmail)
@@ -39,43 +40,60 @@ class LoginViewModelTest {
     @Before
     fun setUp() {
         MockKAnnotations.init(this, relaxUnitFun = true)
-        viewModel = LoginViewModel(userRepository, testDispatchers)
+        viewModel = RegisterViewModel(userRepository, testDispatchers)
         state = viewModel.state
+    }
+
+    @Test
+    fun `onNicknameChange will update email state`() {
+        viewModel.onNicknameChange(stubTestNickname)
+        assertEquals(stubTestNickname, state.value.nickname)
+        assertEquals(false, state.value.isRegisterButtonEnabled)
     }
 
     @Test
     fun `onEmailChange will update email state`() {
         viewModel.onEmailChange(stubTestEmail)
         assertEquals(stubTestEmail, state.value.email)
-        assertEquals(false, state.value.isLoginButtonEnabled)
+        assertEquals(false, state.value.isRegisterButtonEnabled)
     }
 
     @Test
     fun `onPasswordChange will update password state`() {
         viewModel.onPasswordChange(stubTestPassword)
         assertEquals(stubTestPassword, state.value.password)
-        assertEquals(false, state.value.isLoginButtonEnabled)
+        assertEquals(false, state.value.isRegisterButtonEnabled)
     }
 
     @Test
-    fun `isLoginButtonEnabled will be true if both email and password are valid`() {
-        // both valid
+    fun `isRegisterButtonEnabled will be true if all of nickname email and password are valid`() {
+        // all valid
+        viewModel.onNicknameChange(stubTestNickname)
         viewModel.onEmailChange(stubTestEmail)
         viewModel.onPasswordChange(stubTestPassword)
-        assertEquals(true, state.value.isLoginButtonEnabled)
+        assertEquals(true, state.value.isRegisterButtonEnabled)
+
+        // invalid nickname fewer than 3 characters
+        viewModel.onNicknameChange("A")
+        assertEquals(false, state.value.isRegisterButtonEnabled)
+
+        // invalid nickname more than 32 characters
+        viewModel.onNicknameChange("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
+        assertEquals(false, state.value.isRegisterButtonEnabled)
 
         // invalid email
+        viewModel.onNicknameChange(stubTestNickname)
         viewModel.onEmailChange("random_text")
-        assertEquals(false, state.value.isLoginButtonEnabled)
+        assertEquals(false, state.value.isRegisterButtonEnabled)
 
         // invalid password fewer than 6 characters
         viewModel.onEmailChange(stubTestEmail)
         viewModel.onPasswordChange("abc")
-        assertEquals(false, state.value.isLoginButtonEnabled)
+        assertEquals(false, state.value.isRegisterButtonEnabled)
 
         // invalid password more than 32 characters
         viewModel.onPasswordChange("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
-        assertEquals(false, state.value.isLoginButtonEnabled)
+        assertEquals(false, state.value.isRegisterButtonEnabled)
     }
 
     @Test
@@ -87,51 +105,77 @@ class LoginViewModelTest {
     }
 
     @Test
-    fun `onRegisterClick will emit NavigateToRegister effect`() {
+    fun `onBackButtonPressed will emit NavigateBack effect`() {
         runTest {
             viewModel.effect.test {
-                viewModel.onRegisterClick()
-                assertEquals(LoginUIEffect.NavigateToRegister, awaitItem())
+                viewModel.onBackButtonPressed()
+                assertEquals(RegisterUIEffect.NavigateBack, awaitItem())
             }
         }
     }
 
     @Test
-    fun `onLoginClick positive flow will emit LoginSuccess effect`() {
-        coEvery { userRepository.login(stubTestEmail, stubTestPassword) } returns stubTestUser
+    fun `onRegisterClick positive flow will emit RegisterSuccess effect`() {
+        coEvery {
+            userRepository.register(
+                stubTestNickname,
+                stubTestEmail,
+                stubTestPassword
+            )
+        } returns stubTestUser
 
+        viewModel.onNicknameChange(stubTestNickname)
         viewModel.onEmailChange(stubTestEmail)
         viewModel.onPasswordChange(stubTestPassword)
 
         runTest {
             viewModel.effect.test {
-                viewModel.onLoginClick()
-                coVerify { userRepository.login(stubTestEmail, stubTestPassword) }
-                assertEquals(LoginUIEffect.LoginSuccess, awaitItem())
+                viewModel.onRegisterClick()
+                coVerify {
+                    userRepository.register(
+                        stubTestNickname,
+                        stubTestEmail,
+                        stubTestPassword
+                    )
+                }
+                assertEquals(RegisterUIEffect.RegisterSuccess, awaitItem())
                 assertEquals(false, state.value.isLoading)
             }
         }
     }
 
     @Test
-    fun `onLoginClick negative flow will update errorMessage state`() {
+    fun `onRegisterClick negative flow will update errorMessage state`() {
         val stubTestException = spyk<Exception>()
         val stubTestErrorMessage = "Oops"
-        coEvery { userRepository.login(stubTestEmail, stubTestPassword) } throws stubTestException
+        coEvery {
+            userRepository.register(
+                stubTestNickname,
+                stubTestEmail,
+                stubTestPassword
+            )
+        } throws stubTestException
         every { stubTestException.localizedMessage } returns stubTestErrorMessage andThen null
 
+        viewModel.onNicknameChange(stubTestNickname)
         viewModel.onEmailChange(stubTestEmail)
         viewModel.onPasswordChange(stubTestPassword)
 
         runTest {
             viewModel.effect.test {
-                viewModel.onLoginClick()
+                viewModel.onRegisterClick()
                 assertEquals(stubTestErrorMessage, state.value.errorMessage)
 
-                viewModel.onLoginClick()
+                viewModel.onRegisterClick()
                 assertEquals("Something went wrong.", state.value.errorMessage)
 
-                coVerify(exactly = 2) { userRepository.login(stubTestEmail, stubTestPassword) }
+                coVerify(exactly = 2) {
+                    userRepository.register(
+                        stubTestNickname,
+                        stubTestEmail,
+                        stubTestPassword
+                    )
+                }
                 expectNoEvents()
                 assertEquals(false, state.value.isLoading)
             }
