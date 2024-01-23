b720ac2a5d12d6e18015570b20e099ac2e2f3e2f
Zen Dharma <zendharma10@gmail.com>
2024-01-24 00:24:07 +0800

    Add more common stuff into BaseTest

--- app/src/test/java/com/zen/nottwitter/core/BaseTest.kt
+++ app/src/test/java/com/zen/nottwitter/core/BaseTest.kt
@@ -1,12 +1,22 @@
 package com.zen.nottwitter.core
 
+import com.zen.nottwitter.data.model.Post
+import com.zen.nottwitter.data.model.PostConfig
 import com.zen.nottwitter.data.model.User
 import com.zen.nottwitter.data.model.UserConfig
 import io.mockk.MockKAnnotations
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 
+@OptIn(ExperimentalCoroutinesApi::class)
 abstract class BaseTest {
+
+    protected val testDispatchers: TestDispatchers = TestDispatchers()
+
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule(testDispatchers.testDispatcher)
 
     protected val stubTestUserConfig = UserConfig(
         nicknameMinLength = 3,
@@ -17,10 +27,17 @@ abstract class BaseTest {
 
     protected val stubTestFallbackErrorMessage = "Something went wrong."
 
+    protected val stubTestPostConfig = PostConfig(280)
+
     protected val stubTestNickname = "Bob"
     protected val stubTestEmail = "test@gmail.com"
     protected val stubTestPassword = "test-password"
     protected val stubTestUser = User("abc", stubTestNickname, stubTestEmail)
+
+    protected val stubTestPosts = listOf(Post("123"), Post("456"), Post("789"))
+    protected val stubTestPost = Post("wow")
+
+    protected val stubTestImageUrl = "https://coolimage.jpg"
 
     @Before
     open fun setUp() {
