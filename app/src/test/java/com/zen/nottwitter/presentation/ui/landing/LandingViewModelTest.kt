1c557cc4e5a929fc240745cb26a2d4009cfc89a9
Zen Dharma <zendharma10@gmail.com>
2024-01-24 00:24:35 +0800

    Clean up unused codes in LandingViewModelTest

--- app/src/test/java/com/zen/nottwitter/presentation/ui/landing/LandingViewModelTest.kt
+++ app/src/test/java/com/zen/nottwitter/presentation/ui/landing/LandingViewModelTest.kt
@@ -1,33 +1,24 @@
 package com.zen.nottwitter.presentation.ui.landing
 
 import app.cash.turbine.test
+import com.zen.nottwitter.core.BaseTest
 import com.zen.nottwitter.data.model.User
 import com.zen.nottwitter.data.repository.UserRepository
-import com.zen.nottwitter.core.BaseTest
-import com.zen.nottwitter.core.MainCoroutineRule
-import com.zen.nottwitter.core.TestDispatchers
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class LandingViewModelTest : BaseTest() {
 
-    private val testDispatchers: TestDispatchers = TestDispatchers()
     private lateinit var viewModel: LandingViewModel
 
     @MockK
     private lateinit var userRepository: UserRepository
-
-    @get:Rule
-    val mainCoroutineRule = MainCoroutineRule(testDispatchers.testDispatcher)
 
     @Before
     override fun setUp() {
