dcbafc7fef0bd0984b5a9621a1bea034059c08ac
Zen Dharma <zendharma10@gmail.com>
2024-01-24 01:16:23 +0800

    Add test for getDiff in TimeUtils

--- app/src/test/java/com/zen/nottwitter/domain/TimeUtilsTest.kt
+++ app/src/test/java/com/zen/nottwitter/domain/TimeUtilsTest.kt
@@ -5,6 +5,8 @@ import io.mockk.mockk
 import io.mockk.mockkObject
 import io.mockk.unmockkObject
 import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -27,5 +29,15 @@ class TimeUtilsTest {
         val stubTestMillis = 123L
         every { Clock.System.now().toEpochMilliseconds() } returns stubTestMillis
         assertEquals(stubTestMillis, TimeUtils.getCurrentMillis())
+    }
+
+    @Test
+    fun `getDiff will return time diff in String`() {
+        every { Clock.System.now() } returns Instant.fromEpochSeconds(88000)
+        assertEquals("0m", TimeUtils.getDiff(88000))
+        assertEquals("50m", TimeUtils.getDiff(85000))
+        assertEquals("1h", TimeUtils.getDiff(84000))
+        assertEquals("2h", TimeUtils.getDiff(80000))
+        assertEquals("1d", TimeUtils.getDiff(1000))
     }
 }
